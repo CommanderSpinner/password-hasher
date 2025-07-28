ce31ab9efd00a9c0e9d574b481ddc302e683c85d
CommanderSpinner <klaus.kutulu@gmail.com>
2025-07-28 16:54:31 +0200

    updaet

--- hasher.cpp
+++ hasher.cpp
@@ -1,9 +1,29 @@
 #include "hasher.hpp"
 
-Hasher::Hasher(){
+Hasher::Hasher(std::string hash_algo){
     std::cout << "creating hashser \n";
+    this->hash_algo = hash_algo;
 }
 
 Hasher::~Hasher(){
     std::cout << "destroying hashser \n";
 }
+
+void Hasher::hashF()
+{
+    // Create PBKDF2 object
+    Botan::PBKDF* pbkdf = new Botan::PBKDF2(hash.release());
+
+    // Prepare output buffer
+    std::vector<uint8_t> derived_key(key_length);
+
+    // Derive the key (hash the password)
+    pbkdf->derive_key(derived_key.data(), derived_key.size(),
+                      password.data(), password.size(),
+                      salt.data(), salt.size(),
+                      iterations);
+
+    // Print the derived key as hex
+    std::cout << "PBKDF2 hash: " << Botan::hex_encode(derived_key) << "\n";
+}
+
--- hasher.hpp
+++ hasher.hpp
@@ -3,13 +3,21 @@
 #include <string>
 #include <botan-3/botan/hash.h>
 #include <botan-3/botan/hex.h>
+#include <vector>
 
 
 class Hasher{
 private:
     std::string password;
+    std::string hash_algo;
+    std::vector<uint8_t> salt;
+    std::unique_ptr<Botan::HashFunction> hash;
+    size_t iterations;
+    size_t key_length;
+
+    void hashF();
 
 public:
-    Hasher();
+    Hasher(std::string hash_algo);
     ~Hasher();
 };
